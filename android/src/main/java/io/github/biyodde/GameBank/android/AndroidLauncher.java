b41ece89f91db8b4c625ae133d088f7355672861
shware10 <112537781+shware10@users.noreply.github.com>
2024-10-30 04:26:45 +0900

    Initial commit

---
+++ android/src/main/java/io/github/biyodde/GameBank/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package io.github.biyodde.GameBank.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.biyodde.GameBank.Core;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Core(), configuration);
+    }
+}
